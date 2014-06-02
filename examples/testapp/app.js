d30283c8c994cd80e768caf7bc44bf3b762ffe31
Florian Heinemann <florian.heinemann@gmail.com>
2014-06-02 18:56:10 +0200

    Start with an express 4 base app

---
+++ examples/testapp/app.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var debug = require('debug')('simple-mail');
+
+var Passwordless = require('passwordless');
+var MongoStore = require('passwordless-mongostore');
+var email   = require("emailjs");
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// TODO: email setup (has to be changed)
+var yourEmail = 'YOUR MAIL';
+var yourPwd = 'YOUR PWD FOR YOUR MAIL';
+var yourSmtp = 'SMTP FOR YOUR MAIL, e.g.: smtp.gmail.com';
+var smtpServer  = email.server.connect({
+   user:    yourEmail, 
+   password: yourPwd, 
+   host:    yourSmtp, 
+   ssl:     true
+});
+
+// TODO: MongoDB setup (given default can be used)
+var pathToMongoDb = 'mongodb://localhost/passwordless-mongostore-simple-mail';
+
+// TODO: Path to be send via email
+var host = 'http://localhost:3000/';
+
+// Setup of Passwordless
+var passwordless = new Passwordless(new MongoStore(pathToMongoDb));
+passwordless.add(
+    function(contactToVerify, callback) {
+        // Check if given email is valid (always true for this demo)
+        callback(null, contactToVerify);
+    }, 
+    function(tokenToSend, user, callback) {
+        // Send out token
+console.log('sent'); return;
+        server.send({
+           text:    'Hello!\nYou can now access your account here: ' + host + '?token=' + tokenToSend, 
+           from:    yourEmail, 
+           to:      user,
+           subject: 'Token for ' + host
+        }, function(err, message) { 
+            if(err) {
+                console.log(err);
+            }
+            callback(err);
+        });
+    });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Standard express setup
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Passwordless middleware
+app.use(passwordless.sessionSupport());
+app.use(passwordless.acceptToken());
+
+// CHECK /routes/index.js to better understand which routes are needed at a minimum
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
---
+++ examples/testapp/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { user: req.user });
+});
+
+/* GET login screen. */
+router.get('/login', function(req, res) {
+  res.render('login', { user: req.user });
+});
+
+/* GET logout. */
+router.get('/logout', passwordless.logout(),
+	function(req, res) {
+  res.redirect('/');
+});
+
+/* POST login screen. */
+router.post('/sendtoken', passwordless.requestToken(),
+	function(req, res) {
+  res.render('sent');
+});
+
+module.exports = router;
